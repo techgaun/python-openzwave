f3fd7064e0a9a6806f99f1fb7de99bc194345e6c
techgaun <coolsamar207@gmail.com>
2017-06-14 13:48:46 -0500

    update path

--- docs/conf.py
+++ docs/conf.py
@@ -29,9 +29,9 @@ import sys, os
 #sys.path.insert(1, os.path.abspath('../python_openzwave_lib.egg-info'))
 
 if os.environ.get('READTHEDOCS') == "True":
-    sys.path.insert(0, os.path.abspath('../src-api/openzwave'))
-    sys.path.insert(0, os.path.abspath('../src-lib/libopenzwave'))
-    sys.path.insert(0, os.path.abspath('../src-manager/pyozwman'))
+    sys.path.insert(0, os.path.abspath('../src-api/'))
+    sys.path.insert(0, os.path.abspath('../src-lib/'))
+    sys.path.insert(0, os.path.abspath('../src-manager/'))
 sys.path.insert(1, os.path.abspath('..'))
 from pyozw_version import pyozw_version, pyozw_version_short
 
